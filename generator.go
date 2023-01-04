f1efe3cf4b2618ccfb5b2423cc5d65f5300552e6
Хисамов Руслан Равилевич <hisamovrr@sovcombank.ru>
2023-01-04 13:50:02 +0300

    initial and final commit

---
+++ generator.go
@@ -0,0 +1,78 @@
+package kanji_jwt
+
+import (
+	"context"
+	"errors"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"os"
+)
+
+type Generator struct {
+	secretKey string
+}
+
+var (
+	ErrInvalidToken = errors.New("token not valid")
+	ErrNotMapClaims = errors.New("parsedToken.Claims not jwt.MapClaims")
+	ErrIdNotFound   = errors.New("id not found")
+	ErrIsEmpty      = errors.New("jwt field is empty")
+)
+
+func New(secretKey string) (*Generator, error) {
+	return &Generator{secretKey: secretKey}, nil
+}
+
+func (g *Generator) Generate(claims map[string]interface{}) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(claims))
+
+	return token.SignedString([]byte(g.secretKey))
+}
+
+func (g *Generator) ParseToken(token string) (jwt.MapClaims, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(g.secretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if !parsedToken.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, ErrNotMapClaims
+	}
+
+	return claims, nil
+}
+
+func GetUserID(ctx context.Context) (uuid.UUID, error) {
+	jwtToken := ctx.Value("jwt").(string)
+	if len(jwtToken) == 0 {
+		return uuid.UUID{}, ErrIsEmpty
+	}
+
+	g, err := New(os.Getenv("JWT_PASSWORD"))
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	claims, err := g.ParseToken(jwtToken)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	id, ok := claims["user_id"]
+	if !ok {
+		return uuid.UUID{}, ErrIdNotFound
+	}
+
+	userID, err := uuid.Parse(id.(string))
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return userID, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module kanji-jwt
+
+go 1.19
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.3.0
+)
